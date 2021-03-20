7a11307a51bfecb05a711c9408680cbab270112a
aysemadrann <aysemadranesm@gmail.com>
2021-03-21 02:02:10 +0300

    first commit

--- kodluyoruz.js
+++ kodluyoruz.js
@@ -1,9 +1,6 @@
 let user = prompt("İsminizi Giriniz.");
 let firstName = document.querySelector("#username").innerHTML = user;
 let date = new Date();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
 let day = date.getDay();
 let days = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
 var myVar=setInterval(function(){myTimer()},1000);
